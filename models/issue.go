cb4e0b4774b57dd8cdaff6af1e4a9f6aff565893
taco <taco@taco.taco>
2025-06-19 15:59:46 +0300

    pivot to a simpler and more lightweight stack

--- models/issue.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Issue struct {
-	ID          uint64 `gorm:"primary_key;auto_increment" json:"id"`
-	Title       string `json:"title" binding:"min=2,max=100" gorm:"type:varchar(100)"`
-	Description string `json:"title" binding:"min=20,max=500" gorm:"type:varchar(500)"`
-	Severity    int8   `gorm:"type:int8"`
-	User        User   `json:"reporter" binding:"required" gorm:"foreignkey:UserID"`
-}
--- models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-	ID       uint64  `gorm:"primary_key;auto-increment" json:"id"`
-	email    string  `json:"email" binding:"required,email" gorm:"type:varchar(100)"`
-	password string  `json:"email" binding:"required,email" gorm:"type:varchar(100)`
-	issues   []Issue `json:"issues"`
-}
